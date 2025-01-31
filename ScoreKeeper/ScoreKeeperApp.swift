0d1b70a5d70b0fd98ad0784631db8b6f686753f5
Dhruv Naik <dhruviscracked@gmail.com>
2025-01-31 15:34:43 +0530

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Dhruv Naik on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
